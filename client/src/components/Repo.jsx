34aaebbd4e8770703b0deef435a59816ddb7358c
Huan Tran <huanductran98@gmail.com>
2022-08-28 02:03:21 -0700

    Complete repo name linking to github

--- client/src/components/Repo.jsx
+++ client/src/components/Repo.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Repo = (props) => (
   <div>
-    <h4>{props.repo.name}</h4>
+    <a href={props.repo.link} target='_blank'>
+      <h4>{props.repo.name}</h4>
+    </a>
     <ul>
       <li key='username'>Owner: {props.repo.username}</li>
       <li key='description'>Description: {props.repo.description}</li>
       <li key='forks'>Forks: {props.repo.forks_count}</li>
-      <li key='link'>Link: {props.repo.link}</li>
     </ul>
   </div>
 )
